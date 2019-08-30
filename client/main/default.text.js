6ea3f3c4709c9d0d8ffb7013a2e4852746965a16
Rahul Kumar Singh <37482171+rahulkumarsingh73690@users.noreply.github.com>
2019-08-30 16:31:38 +0530

    Update default.text.js

--- client/main/default.text.js
+++ client/main/default.text.js
@@ -7,11 +7,5 @@ people+trying*to%cheat-by|using_symbols=to*split#words is 7 words
 
 But ASCII art or tables are fine
 
-| d4 | Result |
----------------
-| 1  | Sock   |
-| 2  | Shoe   |
-| 3  | Pants  |
-| 4  | Hat    |
 
 is only 10 words`;
